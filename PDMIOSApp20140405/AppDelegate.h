8a22941fa9127675176defa5d8dfa3df99032a2a
dewanmomen <da-momen@hotmail.com>
2014-04-11 02:16:50 +0600

    committed some files

---
+++ PDMIOSApp20140405/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PDMIOSApp20140405
+//
+//  Created by dewan momen on 4/6/14.
+//  Copyright (c) 2014 knocktechbd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PDMIOSApp20140405Tests/PDMIOSApp20140405Tests.m
@@ -0,0 +1,34 @@
+//
+//  PDMIOSApp20140405Tests.m
+//  PDMIOSApp20140405Tests
+//
+//  Created by dewan momen on 4/6/14.
+//  Copyright (c) 2014 knocktechbd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PDMIOSApp20140405Tests : XCTestCase
+
+@end
+
+@implementation PDMIOSApp20140405Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
